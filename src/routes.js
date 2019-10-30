eba4635c2497233fba6bacb189ca05985e279f8e
Vitor Giovane <vitor.giovane@commcepta.com.br>
2019-10-30 10:25:20 -0300

    feature: configura nodemon e sucrase

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express"
+
+const routes = Router()
+
+routes.get("/test", (request, response) => {
+  response.json({ message: "Hello, Gympoint!" })
+})
+
+export default routes
